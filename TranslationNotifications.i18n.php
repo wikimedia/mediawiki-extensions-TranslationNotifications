c0fa8e05dd8d8ac3373188093943e8b929bea8c9
Raimond Spekking <raymond@users.mediawiki.org>
2012-03-03 21:32:20 +0000

    Localisation update for core and extension messages from translatewiki.net

--- TranslationNotifications.i18n.php
+++ TranslationNotifications.i18n.php
@@ -38,16 +38,22 @@ $messages['en'] = array(
 	'translationnotifications-submit' => 'Sign up',
 );
 
+/** Message documentation (Message documentation)
+ * @author Kghbln
+ * @author Siebrand
+ */
 $messages['qqq'] = array(
 	'translatorsignup' => 'Special page header',
 	'translationnotifications-desc' => '{{desc}}',
 	'translationnotifications-info' => 'Fieldset header',
 	'translationnotifications-username' => 'Label followed by username',
 	'translationnotifications-emailstatus' => 'Label',
-	'translationnotifications-email-unconfirmed' => 'Parameters:
+	'translationnotifications-email-confirmed' => 'Status of e-mail confirmation after {{msg-mw|translationnotifications-emailstatus}}.',
+	'translationnotifications-email-unconfirmed' => 'Status of e-mail confirmation after {{msg-mw|translationnotifications-emailstatus}}. Parameters:
 * $1 is a button which can be used to send confirmation email. Button text is {{msg-mw|mediawiki:confirmemail_send}}.',
+	'translationnotifications-email-notset' => 'Status of e-mail confirmation after {{msg-mw|translationnotifications-emailstatus}}.',
 	'translationnotifications-languages' => 'Fieldset header',
-	'translationnotifications-lang' => 'Label for select, $1 is a numbeer',
+	'translationnotifications-lang' => 'Label for select, $1 is a number',
 	'translationnotifications-nolang' => 'First option in a language select',
 	'translationnotifications-contact' => 'Fieldset header',
 	'translationnotifications-cmethod-email' => 'Check option label',
@@ -64,7 +70,49 @@ $messages['qqq'] = array(
 	'translationnotifications-submit' => 'Submit button text',
 );
 
-/** Hebrew
+/** German (Deutsch)
+ * @author Kghbln
+ * @author Metalhead64
+ */
+$messages['de'] = array(
+	'translatorsignup' => 'Übersetzerregistrierung',
+	'translationnotifications-desc' => 'Ermöglicht es Übersetzern Übersetzungsbenachrichtigungen zu abonnieren',
+	'translationnotifications-info' => 'Benutzerinformation',
+	'translationnotifications-username' => 'Benutzername:',
+	'translationnotifications-emailstatus' => 'E-Mail-Status:',
+	'translationnotifications-email-confirmed' => 'Deine E-Mail-Adresse ist bestätigt',
+	'translationnotifications-email-unconfirmed' => 'Deine E-Mail-Adresse ist nicht bestätigt. $1',
+	'translationnotifications-email-notset' => 'Du hast keine E-Mail-Adresse angegeben. Dies kannst du in deinen [[Special:Preferences|Einstellungen]] tun.',
+	'translationnotifications-languages' => 'Sprachen',
+	'translationnotifications-lang' => 'Sprache Nr. $1',
+	'translationnotifications-nolang' => 'Wähle eine Sprache',
+	'translationnotifications-contact' => 'Bevorzugte Benachrichtigungsmethoden',
+	'translationnotifications-cmethod-email' => 'E-Mail',
+	'translationnotifications-cmethod-talkpage' => 'Diskussionsseite',
+	'translationnotifications-cmethod-talkpage-elsewhere' => 'Diskussionsseite auf einem anderen Wiki',
+	'translationnotifications-cmethod-feed' => 'Feed',
+	'translationnotifications-cmethod-no' => 'Benachrichtige mich nicht',
+	'translationnotifications-frequency' => 'Benachrichtigungshäufigkeit',
+	'translationnotifications-freq-always' => 'Immer, wenn neue Übersetzungen vorhanden sind',
+	'translationnotifications-freq-week' => 'Höchstens einmal pro Woche',
+	'translationnotifications-freq-month' => 'Höchstens einmal pro Monat',
+	'translationnotifications-freq-weekly' => 'Wöchentlicher Bericht',
+	'translationnotifications-freq-monthly' => 'Monatlicher Bericht',
+	'translationnotifications-submit' => 'Registrieren',
+);
+
+/** German (formal address) (‪Deutsch (Sie-Form)‬)
+ * @author Kghbln
+ */
+$messages['de-formal'] = array(
+	'translationnotifications-email-confirmed' => 'Ihre E-Mail-Adresse ist bestätigt',
+	'translationnotifications-email-unconfirmed' => 'Ihre E-Mail-Adresse ist nicht bestätigt. $1',
+	'translationnotifications-email-notset' => 'Sie haben keine E-Mail-Adresse angegeben. Dies können Sie in Ihren [[Special:Preferences|Einstellungen]] tun.',
+	'translationnotifications-nolang' => 'Wählen Sie eine Sprache',
+	'translationnotifications-cmethod-no' => 'Benachrichtigen Sie mich nicht',
+);
+
+/** Hebrew (עברית)
  * @author amire80
  */
 $messages['he'] = array(
@@ -77,7 +125,7 @@ $messages['he'] = array(
 	'translationnotifications-email-unconfirmed' => 'הדוא"ל שלך אינו מאומת. $1',
 	'translationnotifications-email-notset' => 'לא נתת כתובת דוא"ל. אפשר לעשות את זה ב[[Special:Preferences|העדפות]] שלך.',
 	'translationnotifications-languages' => 'שפות',
-	'translationnotifications-lang' => 'שפה מס\' $1',
+	'translationnotifications-lang' => "שפה מס' $1",
 	'translationnotifications-nolang' => 'בחירת שפה',
 	'translationnotifications-contact' => 'דרכי התקשרות מועדפות',
 	'translationnotifications-cmethod-email' => 'דוא"ל',
@@ -93,3 +141,184 @@ $messages['he'] = array(
 	'translationnotifications-freq-monthly' => 'סיכום חודשי',
 	'translationnotifications-submit' => 'רישום',
 );
+
+/** Upper Sorbian (Hornjoserbsce)
+ * @author Michawiki
+ */
+$messages['hsb'] = array(
+	'translatorsignup' => 'Registrowanje přełožowarja',
+	'translationnotifications-desc' => 'Zmóžnja registrowanje přełožowarjow za přełožowanske zdźělenja',
+	'translationnotifications-info' => 'Wužiwarske informacije',
+	'translationnotifications-username' => 'Wužiwarske mjeno:',
+	'translationnotifications-emailstatus' => 'E.mejlowy status:',
+	'translationnotifications-email-confirmed' => 'Twoja e-mejlowa adresa je so wobkrućiła',
+	'translationnotifications-email-unconfirmed' => 'Twoja e-mejlowa adresa njeje so wobkrućiła. $1',
+	'translationnotifications-email-notset' => 'Njejsy e-mejlowu adresu podał. Móžoš to w swojich [[Special:Preferences|nastajenjach]] činić.',
+	'translationnotifications-languages' => 'Rěče',
+	'translationnotifications-lang' => 'Rěč čo. $1',
+	'translationnotifications-nolang' => 'Rěč wubrać',
+	'translationnotifications-contact' => 'Preferowane kontaktowanske metody',
+	'translationnotifications-cmethod-email' => 'E-mejl:',
+	'translationnotifications-cmethod-talkpage' => 'Diskusijna strona',
+	'translationnotifications-cmethod-talkpage-elsewhere' => 'Diskusijna strona na druhim wikiju',
+	'translationnotifications-cmethod-feed' => 'Kanal',
+	'translationnotifications-cmethod-no' => 'Mje njekontaktować',
+	'translationnotifications-frequency' => 'Kontaktowa častosć',
+	'translationnotifications-freq-always' => 'Přeco, hdyž je něšto nowe za přełožowanje',
+	'translationnotifications-freq-week' => 'Maksimalnje jónu na tydźeń',
+	'translationnotifications-freq-month' => 'Maksimalnje jónu na měsac',
+	'translationnotifications-freq-weekly' => 'Tydźenska rozprawa',
+	'translationnotifications-freq-monthly' => 'Měsačna rozprawa',
+	'translationnotifications-submit' => 'Registrować',
+);
+
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'translatorsignup' => 'Inscription de traductor',
+	'translationnotifications-desc' => 'Permitte que traductores se inscribe pro notificationes concernente le traduction',
+	'translationnotifications-info' => 'Information de usator',
+	'translationnotifications-username' => 'Nomine de usator:',
+	'translationnotifications-emailstatus' => 'Stato de e-mail:',
+	'translationnotifications-email-confirmed' => 'Tu adresse de e-mail es confirmate',
+	'translationnotifications-email-unconfirmed' => 'Tu adresse de e-mail non es confirmate. $1',
+	'translationnotifications-email-notset' => 'Tu non ha fornite un adresse de e-mail. Tu pote facer isto in le [[Special:Preferences|preferentias]].',
+	'translationnotifications-languages' => 'Linguas',
+	'translationnotifications-lang' => 'Lingua №$1',
+	'translationnotifications-nolang' => 'Selige un lingua',
+	'translationnotifications-contact' => 'Methodos preferite de contacto',
+	'translationnotifications-cmethod-email' => 'E-mail',
+	'translationnotifications-cmethod-talkpage' => 'Pagina de discussion',
+	'translationnotifications-cmethod-talkpage-elsewhere' => 'Pagina de discussion in un altere wiki',
+	'translationnotifications-cmethod-feed' => 'Syndication',
+	'translationnotifications-cmethod-no' => 'Non contactar me',
+	'translationnotifications-frequency' => 'Frequentia de contacto',
+	'translationnotifications-freq-always' => 'Quando il ha qualcosa de nove a traducer',
+	'translationnotifications-freq-week' => 'Al plus un vice per septimana',
+	'translationnotifications-freq-month' => 'Al plus un vice per mense',
+	'translationnotifications-freq-weekly' => 'Digesto septimanal',
+	'translationnotifications-freq-monthly' => 'Digesto mensual',
+	'translationnotifications-submit' => 'Inscriber',
+);
+
+/** Luxembourgish (Lëtzebuergesch)
+ * @author Robby
+ */
+$messages['lb'] = array(
+	'translatorsignup' => 'Aschreiwen als Iwwersetzer',
+	'translationnotifications-desc' => "Erlaabt et Iwwersetzer fir sech op Iwwersetzungs-Noriichten z'abonnéieren",
+	'translationnotifications-info' => 'Benotzerinformatioun',
+	'translationnotifications-username' => 'Benotzernumm:',
+	'translationnotifications-emailstatus' => 'E-Mail-Status:',
+	'translationnotifications-email-confirmed' => 'Är E-Mailadress gouf confirméiert',
+	'translationnotifications-email-unconfirmed' => 'Är E-Mailadress gouf net confirméiert. $1',
+	'translationnotifications-email-notset' => 'Dir hutt keng E-Mailadress uginn. Dir kënnt dat an Ären [[Special:Preferences|Astellunge]] maachen.',
+	'translationnotifications-languages' => 'Sproochen',
+	'translationnotifications-lang' => 'Sprooch #$1',
+	'translationnotifications-nolang' => 'Sicht eng Sprooch eraus',
+	'translationnotifications-contact' => 'Wéi solle mir Iech kontaktéieren?',
+	'translationnotifications-cmethod-email' => 'Mail',
+	'translationnotifications-cmethod-talkpage' => 'Diskussiounssäit',
+	'translationnotifications-cmethod-talkpage-elsewhere' => 'Diskussiounsäit op enger anerer Wiki',
+	'translationnotifications-cmethod-feed' => 'Feed',
+	'translationnotifications-cmethod-no' => 'Kontaktéiert mech net',
+	'translationnotifications-frequency' => 'Heefegkeet vun de Benoriichtigungen',
+	'translationnotifications-freq-always' => "Wann et eppes Neies z'iwwersetze gëtt",
+	'translationnotifications-freq-week' => "Héchstens eemol d'Woch",
+	'translationnotifications-freq-month' => 'Héchstens eemol de Mount',
+	'translationnotifications-freq-weekly' => 'Wëchentleche Resumé',
+	'translationnotifications-freq-monthly' => 'Monatleche Resumé',
+	'translationnotifications-submit' => 'Schreift Iech an',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'translatorsignup' => 'Пријава на преведувач',
+	'translationnotifications-desc' => 'Им овозможува на преведувачите да се пријават за известувања што се однесуваат на преведувањето',
+	'translationnotifications-info' => 'Кориснички податоци',
+	'translationnotifications-username' => 'Корисничко име:',
+	'translationnotifications-emailstatus' => 'Статус на е-пошта:',
+	'translationnotifications-email-confirmed' => 'Вашата е-пошта е потврдена',
+	'translationnotifications-email-unconfirmed' => 'Вашата е-пошта не е потврдена. $1',
+	'translationnotifications-email-notset' => 'Немате наведено е-пошта. Тоа можете да го сторите во вашите [[Special:Preferences|нагодувања]].',
+	'translationnotifications-languages' => 'Јазици',
+	'translationnotifications-lang' => 'Јазик бр. $1',
+	'translationnotifications-nolang' => 'Изберете јазик',
+	'translationnotifications-contact' => 'Претпочитан начин на контакт',
+	'translationnotifications-cmethod-email' => 'Е-пошта',
+	'translationnotifications-cmethod-talkpage' => 'Страница за разговор',
+	'translationnotifications-cmethod-talkpage-elsewhere' => 'Страница за разговор на друго вики',
+	'translationnotifications-cmethod-feed' => 'Канал',
+	'translationnotifications-cmethod-no' => 'Не ме контактирај',
+	'translationnotifications-frequency' => 'Честота на контактот',
+	'translationnotifications-freq-always' => 'Кога ќе се појави нешто ново за преведување',
+	'translationnotifications-freq-week' => 'Највеќе еднаш неделно',
+	'translationnotifications-freq-month' => 'Највеќе еднаш месечно',
+	'translationnotifications-freq-weekly' => 'Неделен преглед',
+	'translationnotifications-freq-monthly' => 'Месечен преглед',
+	'translationnotifications-submit' => 'Пријава',
+);
+
+/** Malay (Bahasa Melayu)
+ * @author Anakmalaysia
+ */
+$messages['ms'] = array(
+	'translatorsignup' => 'Pendaftaran penterjemah',
+	'translationnotifications-desc' => 'Membolehkan penterjemah untuk mendaftar diri untuk pemberitahuan penterjemahan',
+	'translationnotifications-info' => 'Maklumat pengguna',
+	'translationnotifications-username' => 'Nama pengguna:',
+	'translationnotifications-emailstatus' => 'Status e-mel:',
+	'translationnotifications-email-confirmed' => 'Alamat e-mel anda telah disahkan',
+	'translationnotifications-email-unconfirmed' => 'Alamat e-mel anda belum disahkan. $1',
+	'translationnotifications-email-notset' => 'Anda belum menyatakan alamat e-mel. Anda boleh berbuat demikian dalam [[Special:Preferences|keutamaan]] anda.',
+	'translationnotifications-languages' => 'Bahasa',
+	'translationnotifications-lang' => 'Bahasa #$1',
+	'translationnotifications-nolang' => 'Pilih bahasa',
+	'translationnotifications-contact' => 'Kaedah perhubungan yang digemari',
+	'translationnotifications-cmethod-email' => 'E-mel',
+	'translationnotifications-cmethod-talkpage' => 'Laman perbincangan',
+	'translationnotifications-cmethod-talkpage-elsewhere' => 'Laman perbincangan di wiki lain',
+	'translationnotifications-cmethod-feed' => 'Suapan',
+	'translationnotifications-cmethod-no' => 'Jangan hubungi saya',
+	'translationnotifications-frequency' => 'Kekerapan perhubungan',
+	'translationnotifications-freq-always' => 'Apabila ada bahan baru untuk diterjemah',
+	'translationnotifications-freq-week' => 'Paling kerap seminggu sekali',
+	'translationnotifications-freq-month' => 'Paling kerap sebulan sekali',
+	'translationnotifications-freq-weekly' => 'Ikhtisar mingguan',
+	'translationnotifications-freq-monthly' => 'Ikhtisar bulanan',
+	'translationnotifications-submit' => 'Daftar diri',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'translatorsignup' => 'Vertalersregistratie',
+	'translationnotifications-desc' => 'Laat vertalers intekenen voor berichten over vertalingen',
+	'translationnotifications-info' => 'Gebruikersgegevens',
+	'translationnotifications-username' => 'Gebruikersnaam:',
+	'translationnotifications-emailstatus' => 'E-mailadresstatus:',
+	'translationnotifications-email-confirmed' => 'Uw e-mailadres is bevestigd',
+	'translationnotifications-email-unconfirmed' => 'Uw e-mailadres is niet bevestigd. $1',
+	'translationnotifications-email-notset' => 'U hebt geen e-mailadres opgegeven. U kunt dit doen in uw [[Special:Preferences|voorkeuren]].',
+	'translationnotifications-languages' => 'Talen',
+	'translationnotifications-lang' => 'Taal $1',
+	'translationnotifications-nolang' => 'Taal kiezen',
+	'translationnotifications-contact' => 'Gewenste contactmethodes',
+	'translationnotifications-cmethod-email' => 'E-mail',
+	'translationnotifications-cmethod-talkpage' => 'Overlegpagina',
+	'translationnotifications-cmethod-talkpage-elsewhere' => 'Overlegpagina op andere wiki',
+	'translationnotifications-cmethod-feed' => 'Feed',
+	'translationnotifications-cmethod-no' => 'Neem geen contact met mij op',
+	'translationnotifications-frequency' => 'Contactfrequentie',
+	'translationnotifications-freq-always' => 'Wanneer er iets nieuws te vertalen is',
+	'translationnotifications-freq-week' => 'Hoogstens één keer per week',
+	'translationnotifications-freq-month' => 'Hoogstens één keer per maand',
+	'translationnotifications-freq-weekly' => 'Wekelijkse samenvatting',
+	'translationnotifications-freq-monthly' => 'Maandelijkse samenvatting',
+	'translationnotifications-submit' => 'Registreren',
+);
+
