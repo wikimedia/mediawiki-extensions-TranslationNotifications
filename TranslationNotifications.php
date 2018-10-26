f4f3ca2cc681c487751ac0f1593746ea03f4bb20
Umherirrender <umherirrender_de.wp@web.de>
2018-10-26 22:03:08 +0200

    Start showing warning for deprecated PHP entry point
    Change-Id: Ia510576dfd67bf4933b48476a293cb2d57270800

--- TranslationNotifications.php
+++ TranslationNotifications.php
@@ -7,7 +7,12 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	$wgExtensionMessagesFiles['TranslationNotificationsAlias'] =
 		__DIR__ . '/TranslationNotifications.alias.php';
 
+	wfWarn(
+		'Deprecated PHP entry point used for TranslationNotifications extension. ' .
+		'Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
 	return true;
 } else {
-	die( 'This version of TranslationNotifications requires MediaWiki 1.25+' );
+	die( 'This version of the TranslationNotifications requires MediaWiki 1.25+' );
 }
