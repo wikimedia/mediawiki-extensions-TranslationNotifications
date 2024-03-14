9f392a4344514e6f498332c0935eaea71a092b4c
Wangombe <ewangombe-ctr@wikimedia.org>
2024-03-14 08:19:08 +0300

    Special:NotifyTranslators: Use entity selector for selecting message groups
    Loading of message groups using the select field displays a lot of
    results that are hard to seatch. This fix uses the EntitySelector
    to speed up the selection by allowing the user to enter a search term
    that is then debounced.
    
    The selection of discouraged translatable pages is disallowed to keep
    functionality similar to the non-JavaScript experience.
    
    Add a default empty option to the translatable page selector, and
    ensure that one of the translatable page options is selected before
    sending notification.
    
    Bug: T253984
    Depends-on: I16da60e7e8f9d5bdbb6a33059486af23b6c8e782
    Change-Id: Iaceb8e46ae7418612fd959c79a5dd78076c14060

--- resources/ext.translationnotifications.notifytranslators.js
+++ resources/ext.translationnotifications.notifytranslators.js
@@ -27,7 +27,9 @@
 	 */
 	function doPreview() {
 		const uri = new mw.Uri();
-		const translatablePage = $( '#mw-input-tpage :selected' ).text();
+		const $translatablePageDropdown = $( '[name="tpage"]' );
+		const translatablePage = $translatablePageDropdown.find( ':selected' ).text();
+		const translatablePageGroup = $translatablePageDropdown.val();
 		const userName = mw.user.getName();
 		const $priority = $( '#mw-input-wpPriority :selected' );
 		const deadlineDate = infuseElement( 'mw-input-wpDeadlineDate' ).getValue();
@@ -38,7 +40,7 @@
 		uri.path = mw.config.get( 'wgScript' );
 		uri.query = {
 			title: 'Special:Translate',
-			group: 'page-' + translatablePage
+			group: translatablePageGroup
 		};
 
 		let priority = '';
@@ -94,6 +96,94 @@
 				.attr( { id: previewId } )
 				.hide()
 		);
+
+		activateEntitySelector( $( '#mw-tns-group-selector' ) );
+
+		$( '#notifytranslators-form' ).on( 'submit', onSubmit );
+	}
+
+	function activateEntitySelector( $group ) {
+		// hide the message group selector
+		const $groupContainer = $( '.mw-tns-translatable-page-selector' );
+
+		// Change the label, and update the for attribute, and remove the click handler
+		// which causes the entity selector to become un-responsive when triggered
+		$groupContainer
+			.attr( 'for', 'mw-entity-selector-input' )
+			.off( 'click' );
+
+		// Determine what value was set, and set it on the entity selector
+		const selectedGroup = $group.find( 'select option:selected' ).text();
+
+		// load the entity selector and set the value
+		const entitySelector = getEntitySelector( onEntitySelect );
+		entitySelector.setValue( selectedGroup );
+
+		$group.addClass( 'mw-tns-hidden' );
+		$group.after( entitySelector.$element );
+	}
+
+	function onEntitySelect( selectedItem ) {
+		const options = document.querySelectorAll( 'select[name="tpage"] option' );
+		const hasOption = Array.prototype.some.call(
+			options,
+			function ( option ) {
+				return option.value === selectedItem.data;
+			}
+		);
+
+		// Remove existing errors
+		this.$element.parents( '.mw-tns-translatable-page-selector' )
+			.first()
+			.find( '.oo-ui-flaggedElement-error' )
+			.remove();
+		if ( !hasOption ) {
+			// Selected option does not exist, assume that a discouraged message group was selected
+			const errorWidget =
+				new OO.ui.MessageWidget( {
+					type: 'error',
+					inline: true,
+					label: mw.msg( 'translationnotifications-tes-discouraged-group' )
+				} );
+			this.$element.parent().append( errorWidget.$element );
+		}
+
+		// On changing the value using setValue, TextInputWidget has a debounced trigger that removes any validity
+		// flag that's been previously set. See:
+		// https://github.com/wikimedia/oojs-ui/blob/cc5390ef6a2b1f327d0ee1509381f79daba62af9/src/widgets/TextInputWidget.js#L256
+		// setValidityFlag is preferred over using setValidation because setValidation triggers on every key press
+		// making it difficult to determine when an option is selected from the entity selector.
+		setTimeout( () => this.setValidityFlag( hasOption ), 250 );
+
+		const selectedValue = hasOption ? selectedItem.data : '';
+		$( 'select[name="tpage"]' ).val( selectedValue );
+	}
+
+	function onSubmit() {
+		const selectedGroupName = $( 'select[name="tpage"]' ).find( 'option:selected' ).text();
+		const currentVal = $( '.tes-entity-selector' ).find( 'input[type="text"]' ).val();
+
+		// Check if the user has selected a discouraged entity.
+		if ( currentVal !== selectedGroupName ) {
+			mw.notify(
+				mw.msg( 'translationnotifications-tes-discouraged-group', currentVal ),
+				{
+					type: 'error',
+					tag: 'invalid-selection'
+				}
+			);
+			return false;
+		}
+	}
+
+	function getEntitySelector( onSelect ) {
+		const EntitySelectorWidget = require( 'ext.translate.entity.selector' );
+		return new EntitySelectorWidget( {
+			onSelect: onSelect,
+			entityType: [ 'groups' ],
+			groupTypes: [ 'translatable-pages' ],
+			inputId: 'mw-entity-selector-input'
+		} );
 	}
 
 	$( document ).ready( setup );
